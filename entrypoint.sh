1426f195261a53063ba100ad5046b427f916576f
Marek Kulik <mkulik@redhat.com>
2022-04-12 13:49:17 +0200

    ci: fix workflows

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-
-SITEPACKAGES=true tox -vvv -e py310,flake8
-rm -rf .tox/ build/ dist/ modulemd_tools.egg-info/
-find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
